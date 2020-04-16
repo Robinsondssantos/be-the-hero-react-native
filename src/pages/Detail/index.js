ac18783e2cdf4beb09579712468596d4395744f3
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-04-16 13:19:34 -0300

    add axios as api

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
@@ -10,9 +11,22 @@ import styles from './styles';
 export default function Detail() {
 
   const navigation = useNavigation();
+  const message = 'Ola APAD, estou entrando em contato pois gostaria de ajuar no caso "Cadelinha atropelada" com o valor de R$ 120,00';
 
   function navigateBack () {
     navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['robinsondssantos@gmail.com'],
+      body: message
+    })
+  }
+
+  function sendWhatsApp() {
+    Linking.openURL(`whatsapp://send?phone=5546988071441&text=${message}`);
   }
 
   return (
@@ -43,11 +57,11 @@ export default function Detail() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
             <Text style={styles.actionText}>WhatsApp</Text>            
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>            
           </TouchableOpacity>          
         </View>
